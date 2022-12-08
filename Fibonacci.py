551f005b843c4e7163ec0fbd3d8817657afcd486
anu-0035 <116714987+anu-0035@users.noreply.github.com>
2022-12-08 21:41:37 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,15 +1,24 @@
-n=int(input("Enter the number to check: "))
-c=0
-a=1
-b=1
-if n==0 or n==1:
-    print("Yes")
-else:
-    while c<n:
-        c=a+b
-        b=a
-        a=c
-    if c==n:
-        print("Yes,  the number entered is in Fibonacci Series.")
+def check(x):
+    a,b,c=0,1,0
+    if x==0 or x==1:
+        print(x,"YES!")
     else:
-        print("No, the number entered is not in Fibonacci Series.")
+        while c<x:
+            c=a+b
+            
+            b=a
+            
+            a=c
+            
+        if c==x:
+            print(x,":YES!, the number is there in the series.")
+        else:
+            print(x,":NO!, the number is there in the series.")
+a=int(input("How many number u want to enter one number or more than then one number to check in the seriesIf u want to inpur one number enter 1 or else enter any number: "))
+if a==1:
+      n=int(input("enter only one number: "))
+      check(n)
+else:
+    d=list(eval(input("enter more than one number: ")))
+    for i in d:
+        check(i)
